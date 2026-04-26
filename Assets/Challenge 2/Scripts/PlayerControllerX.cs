597759286e5968501bc9e9b93651cd09ff259252
Rogocoijupanulfetelor <indreiangelo2@gmail.com>
2026-04-26 19:29:27 +0300

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    int c = 100;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // On spacebar press, send dog
+        if(c >= 100)
+        {
+           if (Input.GetKeyDown(KeyCode.Space))
+           {
+               Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+               c=0;
+           }
+        }
+        c++;
+    }
+}
